b32338d77bbe6c68375dceabfa37d99144a8e778
Heron Rodrigues <heronrs1@gmail.com>
2021-06-05 23:10:22 -0300

    Try to fix project build
    add imports from real input component

--- src/components/shared/input-test/index.tsx
+++ src/components/shared/input-test/index.tsx
@@ -1,3 +1,16 @@
+import React, {
+    InputHTMLAttributes,
+    useEffect,
+    useRef,
+    useState,
+    useCallback,
+} from 'react';
+import { useField } from '@unform/core';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+    name: string;
+}
+
 export default function InputTest() {
     return (
         <h1>InputTest</h1>
